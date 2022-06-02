019801ea008510cca55f357a2839f0011b88a8f5
Dhiraj Eadara <eadaradhiraj@gmail.com>
2022-06-03 00:05:17 +0530

    add repr

--- declension.py
+++ declension.py
@@ -1,7 +1,7 @@
 from operator import ge
 import warnings
 
-numbers_or_cases = {
+NUMBERS_OR_CASES = {
     'singular': 'ekavacana',
     'dual': 'dvivacana',
     'plural': 'bahuvacana',
@@ -9,7 +9,9 @@ numbers_or_cases = {
     'accusative': 'dvitIyA',
     'instrumental': 'tRtIyA',
 }
-genders = {
+NUMBERS = ('singular', 'dual', 'plural')
+CASES = ('nominative', 'accusative', 'instrumental')
+GENDERS = {
     'male': ('male',  'm', 'M', 'Male', 'MALE', 'pulliGga'),
     'female': ('female', 'f', 'F', 'Female', 'FEMALE', 'strIliGga'),
     'neuter': ('neuter', 'n', 'N', 'Neuter', 'NEUTER', 'napuMsakaliGga')
@@ -22,7 +24,7 @@ class subcls:
     def __init__(self, **kwargs) -> None:
         self.__kwargs = kwargs
         for k in kwargs:
-            for _k in [k, numbers_or_cases[k]]:
+            for _k in [k, NUMBERS_OR_CASES[k]]:
                 setattr(self, _k, kwargs[k])
         self.__initialized = True
 
@@ -62,9 +64,15 @@ class Declension:
 
     def __init__1__(self) -> None:
         if self.noun.endswith('a'):
-            if self.gender in genders['male']:
+            if self.gender in GENDERS['male']:
+
                 self.nominative = self.prathamA = subcls(
                     singular='rAmaH', dual='rAmau', plural='rAmAH')
+                self.accusative = self.divitiyA = subcls(
+                    singular='rAmam', dual='rAmau', plural='rAmAn')
+                self.instrumental = self.tRtiyA = subcls(
+                    singular='rAmeNa', dual='rAmAbhyAm', plural='rAmaiH')
+
                 self.singular = self.ekavacana = subcls(
                     nominative='rAmaH', accusative='rAmam', instrumental='rAmeNa')
                 self.dual = self.dvivacana = subcls(
@@ -100,7 +108,62 @@ class Declension:
     def __delattr__(self, _) -> None:
         warnings.warn('Deletion Not Allowed!')
 
+    def _get_case_items(self, case) -> dict:
+        return {num: self[case][num] for num in NUMBERS}
 
-# a_dec = Declension(noun='rAMa', gender='m')
-# del a_dec.singular
-# print(a_dec)
+    def _get_all_case_items(self) -> dict:
+        return {case: self._get_case_items(case) for case in CASES}
+
+    def _get_all_num_items(self) -> dict:
+        return {num: self._get_num_items(num) for num in NUMBERS}
+
+    def _get_num_items(self, num) -> dict:
+        return {case: self[num][case] for case in CASES}
+
+    def to_dict(self, orient='case') -> dict:
+        if orient == 'case':
+            return self._get_all_case_items()
+        elif orient == 'number':
+            return self._get_all_num_items()
+        else:
+            raise AttributeError
+
+    def __repr__(self) -> str:
+        case_dict = self.to_dict(orient='case')
+        max_case_width = max([len(c) for c in CASES]) + 3
+        num_columns = list(list(case_dict.values())[0].keys())
+        widths = [max_case_width]+[
+            max(
+                [
+                    [len(sv) for sv in val.values()][idx]
+                    for val in case_dict.values()
+                ] + [len(col)]
+            )+5 for idx, col in enumerate(num_columns)
+        ]
+        num_columns = [""]+num_columns
+
+        col_format = []
+        for w in widths:
+            if len(col_format) == 0:
+                col_format.append("%s")
+            else:
+                col_format.append("%" + str(w) + "s")
+        str_col_format = " ".join(col_format)
+        for ci in range(len(num_columns)):
+            num_columns[ci] = f"{num_columns[ci].ljust(widths[ci])}"
+        res_str = (str_col_format % tuple(num_columns))
+        res_str += '\n'
+        res_str += len(res_str)*'-'
+        res_str += '\n'
+        for idx, vals in enumerate(case_dict.values()):
+            values = [CASES[idx]]+list(vals.values())
+            for vi in range(len(values)):
+                values[vi] = f"{values[vi].ljust(widths[vi])}"
+            res_str += (str_col_format % tuple(values))
+            res_str += '\n'
+
+        return res_str
+
+
+a_dec = Declension(noun='rAMa', gender='m')
+print(a_dec)
--- test_declension.py
+++ test_declension.py
@@ -1,4 +1,5 @@
 
+#pytest -v
 import declension
 import pytest
 
